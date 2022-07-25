8a0da1fb6478e57e6d55850241c028a8932afe15
Angélica Martins <marttinsangelica@gmail.com>
2022-07-25 09:02:18 -0300

    Initial commit

---
+++ src/Types/Types.ts
@@ -0,0 +1,24 @@
+export type Data = {
+  currentUser: UserType
+  comments: CommentsType[]
+}
+
+export type UserType = {
+  image: ImageType
+  username: string
+}
+
+export type ImageType = {
+  png: string
+  webp: string
+}
+
+export type CommentsType = {
+  id: number
+  content: string
+  createdAt: string
+  score: number
+  user: UserType
+  replyingTo?: string
+  replies?: CommentsType[]
+}
