32eb0f8af6792fc16304845af39a1db46b1fd9fb
Angélica Martins <marttinsangelica@gmail.com>
2022-08-15 14:31:09 -0300

    Chore: Add assets folder and refactor the URL in data.json

--- src/Components/Comments/Comments.tsx
+++ src/Components/Comments/Comments.tsx
@@ -10,13 +10,14 @@ export const Comments = ({
 }: CommentsType) => {
 	const { image, username } = user
 	const { png, webp } = image
+
 	return (
 		<>
+			<img src={png} alt={`${username} photo`} />
 			<p>{id}</p>
 			<p>{createdAt}</p>
 			<p>{content}</p>
 			<p>{score}</p>
-      <img src={png} alt="Photo" />
 		</>
 	)
 }
