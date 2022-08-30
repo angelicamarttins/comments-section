8bcb553698a58f9af0ffe621ec617c5b29d30215
Angélica Martins <marttinsangelica@gmail.com>
2022-08-29 21:54:09 -0300

    Refactor: Reorganize types in Types.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
+import { useEffect } from 'react'
 import { Comments } from './Components/Comments/Comments'
 import { NewComment } from './Components/NewComment/NewComment'
+import { requestProps, useFetch } from './Hooks/useFetch'
 import useLocalStorage from './Hooks/useLocalStorage'
-import { CommentsType } from './Types/Types'
+import { CommentsData, CommentsType } from './Types/Types'
 
 function App() {
-	const { commentsData, addLocalStorageValues } = useLocalStorage()
-	console.log('app')
+	const {
+		data: commentsData,
+		request
+	}: {
+		data?: CommentsData
+		request: ({ url }: requestProps) => Promise<void>
+	} = useFetch()
 
+	useEffect(() => {
+		request({ url: 'data.json' })
+	}, [])
+
+	console.log(commentsData)
 	return (
 		<>
-			<button onClick={() => addLocalStorageValues('teste', { teste: 123 })}>
+			{/* <button onClick={() => addLocalStorageValues('teste', { teste: 123 })}>
 				Teste
-			</button>
+			</button> */}
 			{commentsData &&
 				commentsData.comments.map((comment: CommentsType, index) => {
 					return <Comments index={index} key={comment.id} {...comment} />
--- src/Types/Types.ts
+++ src/Types/Types.ts
@@ -1,25 +1,25 @@
-export type CommentsData = {
-  currentUser: UserType
-  comments: CommentsType[]
+export type CommentsType = {
+	id: number
+	index?: number
+	content: string
+	createdAt: string
+	score: number
+	user: UserType
+	replyingTo?: string
+	replies?: CommentsType[]
 }
 
 export type UserType = {
-  image: ImageType
-  username: string
+	image: ImageType
+	username: string
 }
 
 export type ImageType = {
-  png: string
-  webp: string
+	png: string
+	webp: string
 }
 
-export type CommentsType = {
-  id: number
-  index?: number
-  content: string
-  createdAt: string
-  score: number
-  user: UserType
-  replyingTo?: string
-  replies?: CommentsType[]
+export type CommentsData = {
+	currentUser: UserType
+	comments: CommentsType[]
 }
